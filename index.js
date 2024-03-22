ac76869656e0e0484eb813671d52fa1fbbce3dc1
Kristiann001 <162113593+Kristiann001@users.noreply.github.com>
2024-03-22 23:00:51 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution here!
+const cats = ["Milo","Otis","Garfield"]
+
+const destructivelyAppendCat = ["Milo","Otis","Garfield"]
+destructivelyAppendCat.push("Michael")
+
+const destructivelyPrependCat = ["Milo","Otis","Garfield"]
+destructivelyPrependCat.unshift("Bob")
+
+const destructivelyRemoveLastCat = ["Milo","Otis","Garfield"]
+destructivelyRemoveLastCat.pop()
+
+const destructivelyRemoveFirstCat = ["Milo","Otis","Garfield"]
+destructivelyRemoveFirstCat.shift()
+
+const appendCat = ["Milo","Otis","Garfield"]
+appendCat.push("Broom")
+
+const prependCat = ["Milo","Otis","Garfield"]
+prependCat.unshift("Arnold")
+
+const removeLastCat = ["Milo","Otis","Garfield"]
+removeLastCat.slice(0,-1)
+
+const removeFirstCat = ["Milo","Otis","Garfield"]
+removeFirstCat.slice(1)
